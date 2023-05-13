aeeb5344ce387bc5fd98e3a6586a480f59b9c54c
GabrielHall02 <ghallabreu@gmail.com>
2023-05-13 10:45:04 +0100

    Starting frontend

---
+++ platform/frontend/src/scenes/acp/Chart.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
+import Title from '../../components/Title.tsx';
+
+// Generate Sales Data
+function createData(time: string, amount?: number) {
+  return { time, amount };
+}
+
+const data = [
+  createData('01-05-2023', 0),
+  createData('04-05-2023', 3),
+  createData('05-05-2023', 6),
+  createData('07-05-2023', 2),
+  createData('10-05-2023', 9),
+];
+
+export default function Chart() {
+  const theme = useTheme();
+
+  return (
+    <React.Fragment>
+      <Title>Today</Title>
+      <ResponsiveContainer>
+        <LineChart
+          data={data}
+          margin={{
+            top: 16,
+            right: 16,
+            bottom: 0,
+            left: 24,
+          }}
+        >
+          <XAxis
+            dataKey="time"
+            stroke={theme.palette.text.secondary}
+            style={theme.typography.body2}
+          />
+          <YAxis
+            stroke={theme.palette.text.secondary}
+            style={theme.typography.body2}
+          >
+            <Label
+              angle={270}
+              position="left"
+              style={{
+                textAnchor: 'middle',
+                fill: theme.palette.text.primary,
+                ...theme.typography.body1,
+              }}
+            >
+              Orders
+            </Label>
+          </YAxis>
+          <Line
+            isAnimationActive={true}
+            type="monotone"
+            dataKey="amount"
+            stroke={theme.palette.primary.main}
+            dot={true}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </React.Fragment>
+  );
+}
