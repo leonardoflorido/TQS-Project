c671b76e6d499e7465b73760d8634c37befa2cc2
Leonardo Flórido <leonardo.s.florido@outlook.com>
2023-05-26 12:02:34 +0100

    Register test done.

--- platform/backend/src/test/java/platform/backend/controller/PickupControllerTest.java
+++ platform/backend/src/test/java/platform/backend/controller/PickupControllerTest.java
@@ -1,8 +1,10 @@
 package platform.backend.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,11 +14,15 @@ import platform.backend.model.Pickup;
 import platform.backend.service.PickupService;
 import platform.backend.utils.JsonUtil;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureDataMongo
 class PickupControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -24,16 +30,26 @@ class PickupControllerTest {
     @MockBean
     private PickupService pickupService;
 
+    private Pickup pickup;
+
+    @BeforeEach
+    void setUp() {
+        pickup = new Pickup("Tabacaria", "tabacaria@email.com", "987654321", "tabacaria", "Avenida Doutor Lourenço Peixinho, 3810-123, Aveiro", "Pending");
+    }
+
     @Test
     @DisplayName("Test to register a pickup with a valid input")
     void testRegisterPickupWithValidInput() throws Exception {
-        Pickup pickup = new Pickup("Tabacaria", "tabacaria@email.com", "987654321", "tabacaria", "Avenida Doutor Lourenço Peixinho, 3810-123, Aveiro", "Pending");
-
-        System.out.println(pickupService.save(pickup));
+        when(pickupService.save(any(Pickup.class))).thenReturn(pickup);
 
         mockMvc.perform(post("/pickup/register")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(JsonUtil.toJson(pickup)))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value(pickup.getName()))
+                .andExpect(jsonPath("$.email").value(pickup.getEmail()))
+                .andExpect(jsonPath("$.phone").value(pickup.getPhone()))
+                .andExpect(jsonPath("$.address").value(pickup.getAddress()))
+                .andExpect(jsonPath("$.status").value(pickup.getStatus()));
     }
 }
