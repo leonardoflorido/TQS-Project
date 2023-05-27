bc5cd825bac7bd99b1b3b548aed76d1944f95512
GabrielHall02 <ghallabreu@gmail.com>
2023-05-27 18:00:04 +0100

    update on frontend

--- platform/frontend/src/scenes/acp/RecentOrders.jsx
+++ platform/frontend/src/scenes/acp/RecentOrders.jsx
@@ -1,24 +1,68 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import Title from '../../components/Title';
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Title from "../../components/Title";
 
 export default function Deposits() {
-  const [data, setData] = React.useState({});
-  React.useEffect(() => {
-    // Fetch pickup orders from api
-    // Filter the most recent date
-    // Sum the total of orders
-    // setData({date, totalOrders})
-  }, []);
-  return (
-    <React.Fragment>
-      <Title>Recent Orders</Title>
-      <Typography component="p" variant="h4">
-        7
-      </Typography>
-      <Typography color="text.secondary" sx={{ flex: 1 }}>
-        on 12 May, 2023
-      </Typography>
-    </React.Fragment>
-  );
+	const [data, setData] = React.useState({});
+	React.useEffect(() => {
+		// Fetch pickup orders
+		async function fetchOrders() {
+			const response = await fetch(
+				`http://localhost:8080/order/get_orders?pickupId=${localStorage.getItem(
+					"pickupId"
+				)}`,
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+			const data = await response.json();
+			// Map data to return an array of objects with the date and the number of orders
+			// Date should only consider the day, month and year
+			// Example: {date: "2023-10-01", totalOrders: 2}
+			// The final array should be sorted by date
+
+			const orders = data;
+			const ordersByDate = orders.reduce((acc, order) => {
+				const date = order.date.split("T")[0];
+				if (acc[date]) {
+					acc[date]++;
+				} else {
+					acc[date] = 1;
+				}
+				return acc;
+			}, {});
+			const ordersByDateArray = Object.keys(ordersByDate).map((date) => {
+				return { date, totalOrders: ordersByDate[date] };
+			});
+			const sortedOrdersByDateArray = ordersByDateArray.sort((a, b) => {
+				const dateA = new Date(a.date);
+				const dateB = new Date(b.date);
+				return dateA - dateB;
+			});
+
+			const date =
+				sortedOrdersByDateArray[sortedOrdersByDateArray.length - 1]
+					.date;
+			const totalOrders =
+				sortedOrdersByDateArray[sortedOrdersByDateArray.length - 1]
+					.totalOrders;
+			setData({ date, totalOrders });
+		}
+		fetchOrders();
+	}, []);
+  
+	return (
+		<React.Fragment>
+			<Title>Recent Orders</Title>
+			<Typography component="p" variant="h4">
+				{data.totalOrders}
+			</Typography>
+			<Typography color="text.secondary" sx={{ flex: 1 }}>
+				on {data.date}
+			</Typography>
+		</React.Fragment>
+	);
 }
--- platform/frontend/src/scenes/dashboard/Chart.jsx
+++ platform/frontend/src/scenes/dashboard/Chart.jsx
@@ -1,74 +1,143 @@
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
-import Title from "../../components/Title"
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import {
+	LineChart,
+	Line,
+	XAxis,
+	YAxis,
+	Label,
+	ResponsiveContainer,
+} from "recharts";
+import Title from "../../components/Title";
 
 // Generate Sales Data
 function createData(time, amount) {
-  return { time, amount };
+	return { time, amount };
 }
 
 const data = [
-  createData('01-05-2023', 0),
-  createData('04-05-2023', 3),
-  createData('05-05-2023', 6),
-  createData('07-05-2023', 2),
-  createData('10-05-2023', 9),
+	createData("01-05-2023", 0),
+	createData("04-05-2023", 3),
+	createData("05-05-2023", 6),
+	createData("07-05-2023", 2),
+	createData("10-05-2023", 9),
 ];
 
 export default function Chart() {
-  React.useEffect(() => {
-    const fetchData = async () => {
-      // TODO: fetch data from API
-    };
-    fetchData();
-    
-  }, []);
-  const theme = useTheme();
+	const [data, setData] = React.useState([]);
 
-  return (
-    <React.Fragment>
-      <Title>Today</Title>
-      <ResponsiveContainer>
-        <LineChart
-          data={data}
-          margin={{
-            top: 16,
-            right: 16,
-            bottom: 0,
-            left: 24,
-          }}
-        >
-          <XAxis
-            dataKey="time"
-            stroke={theme.palette.text.secondary}
-            style={theme.typography.body2}
-          />
-          <YAxis
-            stroke={theme.palette.text.secondary}
-            style={theme.typography.body2}
-          >
-            <Label
-              angle={270}
-              position="left"
-              style={{
-                textAnchor: 'middle',
-                fill: theme.palette.text.primary,
-                ...theme.typography.body1,
-              }}
-            >
-              Orders
-            </Label>
-          </YAxis>
-          <Line
-            isAnimationActive={true}
-            type="monotone"
-            dataKey="amount"
-            stroke={theme.palette.primary.main}
-            dot={true}
-          />
-        </LineChart>
-      </ResponsiveContainer>
-    </React.Fragment>
-  );
+  async function fetchPickups() {
+    let pickups = [];
+    const response = await fetch(
+      "http://localhost:8080/pickup/get_all",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    for (let i = 0; i < data.length; i++) {
+      pickups.push(data[i].id);
+    }
+    return pickups;
+  }
+
+  async function fetchOrders(pickups) {
+    let orders = [];
+    pickups.forEach(async (id) => {
+      const response = await fetch(
+        `http://localhost:8080/order/get_orders?pickupId=${id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      orders.push(data);
+    });
+    return orders;
+  }
+
+	React.useEffect(() => {
+    fetchPickups().then((pickups) => { fetchOrders(pickups) }).then((orders)=> console.log(orders));
+    const orders = [];
+		// Fetch pickups
+		async function mapOrders() {
+			
+      console.log(orders);
+
+			const ordersByDate = orders.reduce((acc, order) => {
+				const date = order.date.split("T")[0];
+				if (acc[date]) {
+					acc[date]++;
+				} else {
+					acc[date] = 1;
+				}
+				return acc;
+			}, {});
+			const ordersByDateArray = Object.keys(ordersByDate).map((date) => {
+				return { date, totalOrders: ordersByDate[date] };
+			});
+			const sortedOrdersByDateArray = ordersByDateArray.sort((a, b) => {
+				const dateA = new Date(a.date);
+				const dateB = new Date(b.date);
+				return dateA - dateB;
+			});
+			console.log(sortedOrdersByDateArray);
+			setData(sortedOrdersByDateArray);
+		}
+		mapOrders();
+	}, []);
+
+	const theme = useTheme();
+
+	return (
+		<React.Fragment>
+			<Title>Today</Title>
+			<ResponsiveContainer>
+				<LineChart
+					data={data}
+					margin={{
+						top: 16,
+						right: 16,
+						bottom: 0,
+						left: 24,
+					}}
+				>
+					<XAxis
+						dataKey="date"
+						stroke={theme.palette.text.secondary}
+						style={theme.typography.body2}
+					/>
+					<YAxis
+						stroke={theme.palette.text.secondary}
+						style={theme.typography.body2}
+					>
+						<Label
+							angle={270}
+							position="left"
+							style={{
+								textAnchor: "middle",
+								fill: theme.palette.text.primary,
+								...theme.typography.body1,
+							}}
+						>
+							Orders
+						</Label>
+					</YAxis>
+					<Line
+						isAnimationActive={true}
+						type="monotone"
+						dataKey="totalOrders"
+						stroke={theme.palette.primary.main}
+						dot={true}
+					/>
+				</LineChart>
+			</ResponsiveContainer>
+		</React.Fragment>
+	);
 }
