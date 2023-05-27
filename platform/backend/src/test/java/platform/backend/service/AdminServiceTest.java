248a24ee4445db6ecc2a06e2bedc33d4f1421506
Leonardo Flórido <leonardo.s.florido@outlook.com>
2023-05-27 18:21:37 +0100

    Reformat the code.

--- platform/backend/src/test/java/platform/backend/service/AdminServiceTest.java
+++ platform/backend/src/test/java/platform/backend/service/AdminServiceTest.java
@@ -10,6 +10,7 @@ import platform.backend.repository.AdminRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -20,32 +21,19 @@ class AdminServiceTest {
     @InjectMocks
     private AdminService adminService;
 
+    private Admin admin;
+
     @BeforeAll
     void setUp() {
         MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    @DisplayName("Test to find admin by email")
-    public void testFindByEmail() {
-        // Arrange
-        String email = "admin@example.com";
-        Admin admin = new Admin("admin", email, "admin");
-        when(adminRepository.findByEmail(email)).thenReturn(admin);
-
-        // Act
-        Admin result = adminService.findByEmail(email);
-
-        // Assert
-        assertEquals(admin, result);
-        verify(adminRepository, times(1)).findByEmail(email);
+        admin = new Admin("admin", "admin@email.com", "admin");
     }
 
     @Test
     @DisplayName("Test to save admin")
+    @Order(1)
     public void testSave() {
         // Arrange
-        Admin admin = new Admin("admin", "admin@admin.com", "admin");
         when(adminRepository.save(admin)).thenReturn(admin);
 
         // Act
@@ -54,6 +42,22 @@ class AdminServiceTest {
         // Assert
         assertEquals(admin, result);
         verify(adminRepository, times(1)).save(admin);
+    }
+
+    @Test
+    @DisplayName("Test to find admin by email")
+    @Order(2)
+    public void testFindByEmail() {
+        // Arrange
+        String email = "admin@email.com";
+        when(adminRepository.findByEmail(email)).thenReturn(admin);
+
+        // Act
+        Admin result = adminService.findByEmail(email);
+
+        // Assert
+        assertEquals(admin, result);
+        verify(adminRepository, times(1)).findByEmail(email);
     }
 }
 
