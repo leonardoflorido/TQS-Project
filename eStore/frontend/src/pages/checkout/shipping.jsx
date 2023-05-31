ab671138c974abbfff13527565f34fca90ee95d8
tomascrl <tomasalmeida20028@gmail.com>
2023-05-31 15:45:11 +0100

    POST to plattform #TODO

--- eStore/frontend/src/pages/checkout/shipping.jsx
+++ eStore/frontend/src/pages/checkout/shipping.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./shipping.css";
-import { setSelectionRange } from "@testing-library/user-event/dist/utils";
 
-export const ShippingForm = ({setSelectedTab}) => {
+export const ShippingForm = ({ setSelectedTab }) => {
   const [deliveryOption, setDeliveryOption] = useState("pickup");
   const [pickupPoints, setPickupPoints] = useState([]);
+  const [selectedPickupPoint, setSelectedPickupPoint] = useState(null);
 
   useEffect(() => {
     if (deliveryOption === "pickup") {
@@ -21,67 +21,41 @@ export const ShippingForm = ({setSelectedTab}) => {
       console.error("Error fetching pickup points:", error);
     }
   };
+
   const handleTabChange = () => {
     setSelectedTab("tab3");
-    };
+  };
+
+  const handlePickupPointChange = (event) => {
+    const selectedPointId = event.target.value;
+    const selectedPoint = pickupPoints.find(
+      (point) => point.id === selectedPointId
+    );
+    setSelectedPickupPoint(selectedPoint);
+  };
 
   return (
     <div className="shipping-container">
       <form className="shipping-form">
-        <div>
-          <input
-            type="radio"
-            id="pickup"
-            name="delivery"
-            value="pickup"
-            checked={deliveryOption === "pickup"}
-            onChange={() => setDeliveryOption("pickup")}
-          />
-          <label
-            htmlFor="pickup"
-            className={
-              deliveryOption === "pickup" ? "active" : "shipping-label"
-            }
-          >
-            Pickup
-          </label>
-        </div>
-        <div>
-          <input
-            type="radio"
-            id="standard"
-            name="delivery"
-            value="standard"
-            checked={deliveryOption === "standard"}
-            onChange={() => setDeliveryOption("standard")}
-          />
-          <label
-            htmlFor="standard"
-            className={
-              deliveryOption === "standard" ? "active" : "shipping-label"
-            }
-          >
-            Standard Delivery
-          </label>
-        </div>
-        {deliveryOption === "standard" && (
-          <div className="shipping-information">
-            <h3>Shipping Information</h3>
-            <input type="text" placeholder="Full Name" />
-            <input type="text" placeholder="Address" />
-            <input type="text" placeholder="City" />
-            <input type="text" placeholder="State" />
-            <input type="text" placeholder="Zip Code" />
-          </div>
-        )}
+        {/* Radio buttons for delivery options */}
+        {/* Shipping information inputs */}
         {deliveryOption === "pickup" && pickupPoints.length > 0 && (
           <div className="pickup-points">
             <h3>Pickup Points</h3>
-            <select>
+            {/* Render pickup points in a dropdown */}
+            <select onChange={handlePickupPointChange}>
+              <option value="">Select Pickup Point</option>
               {pickupPoints.map((pickupPoint) => (
-                <option key={pickupPoint.id}>{pickupPoint.name}</option>
+                <option key={pickupPoint.id} value={pickupPoint.id}>
+                  {pickupPoint.name}
+                </option>
               ))}
             </select>
+            {selectedPickupPoint && (
+              <div className="pickup-address">
+                <h4>Address: {selectedPickupPoint.address}</h4>
+              </div>
+            )}
           </div>
         )}
       </form>
