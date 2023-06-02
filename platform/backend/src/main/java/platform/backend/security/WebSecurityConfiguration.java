eec450cc4cc8844e3d80b5b6d34b0dae8fa815c7
Leonardo Flórido <leonardo.s.florido@outlook.com>
2023-06-02 09:08:34 +0100

    Update.

--- platform/backend/src/main/java/platform/backend/security/WebSecurityConfiguration.java
+++ platform/backend/src/main/java/platform/backend/security/WebSecurityConfiguration.java
@@ -8,7 +8,7 @@ public class WebSecurityConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://34.175.80.212:3000/", "http://34.175.80.212:3001/", "http://localhost:3000/", "http://localhost:3000/", "http://34.175.80.212:3000", "http://34.175.80.212:3001")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH");
     }
 }
