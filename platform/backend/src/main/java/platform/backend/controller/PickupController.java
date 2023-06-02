19720210e32e9c85bfa6f1066db78b34355a8523
tomascrl <tomasalmeida20028@gmail.com>
2023-06-02 01:52:01 +0100

    idk man

--- platform/backend/src/main/java/platform/backend/controller/PickupController.java
+++ platform/backend/src/main/java/platform/backend/controller/PickupController.java
@@ -104,6 +104,7 @@ public class PickupController {
     }
 
     @GetMapping("/get-partners")
+    @CrossOrigin(origins = {"http://localhost:3000", "http://34.175.80.212:3000"})
     public ResponseEntity<List<Pickup>> getPartners() {
         List<Pickup> pickups = pickupService.findAllByStatus("Partner");
 
