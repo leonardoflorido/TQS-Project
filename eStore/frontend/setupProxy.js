493717b3d112ce836a3c84807ffd428b6d2420b6
tomascrl <tomasalmeida20028@gmail.com>
2023-06-02 02:40:53 +0100

    not gonna work

--- eStore/frontend/setupProxy.js
+++ eStore/frontend/setupProxy.js
@@ -2,9 +2,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api',
     createProxyMiddleware({
-      target: 'http://34.175.80.212:8080',  // Replace with your backend server URL
+      target: 'http://34.175.80.212:8080',
+      pathRewrite: {
+        '^/api': '', // Remove the '/api' prefix from the request
+      },
       changeOrigin: true,
     })
   );
